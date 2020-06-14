845ef063f771d79d3957ecc4a1e398c8c168cad7
eugenechia95 <39354695+eugenechia95@users.noreply.github.com>
2020-06-14 20:49:36 +0800

    refactored request params for train endpoint and fixed directory bug (#1)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, url_for, flash, jsonify
-from helper_functions import optimize_floats, optimize_memory
+from helper_functions import *
 from core_functions import rms_pricing_model
 import numpy as np
 import pickle as p
@@ -10,71 +10,72 @@ from pathlib import Path
 
 
 app = Flask(__name__)
+	
 
 @app.route('/train/', methods=['POST'])
 def train():
-    data = json.loads(request.json['data'])
-    cv_acc = request.get_json()['cv_acc']
-    project_id = request.get_json()['project_id']
 
-    response_outp = {'result':0,
-    				 'cv_acc':0
-    				}
-    # print(data)
-    data_df = pd.DataFrame().from_dict(data)
-    pdm = rms_pricing_model(data_df)
-    pdm.train_all_items(retrain=True)
-    item_ids = [int(x.split('_')[1]) for x in pdm.price_columns]
-    for item_id in item_ids:
-    	item_model = pdm.models[item_id]
-    	MODEL_PATH = 'projects/{}/models/'.format(project_id)
-    	if not os.path.isdir(MODEL_PATH):
-    		Path(MODEL_PATH).mkdir(parents=True)
-    	p.dump(item_model, open('model_{}.p'.format(project_id,item_id),'wb'))
-    response_outp['result'] = 'Success'
-    
-    if cv_acc == True:
-    	perf_df = pdm.get_all_performance()
-    	response_outp['cv_acc'] = perf_df.to_json()
+	data, cv_acc, project_id = parse_training_request(request)
 
-    return jsonify(response_outp)
+	response_outp = {'result':0,
+					 'cv_acc':0
+					}
+					
+	data_df = pd.DataFrame().from_dict(data)
+	pdm = rms_pricing_model(data_df)
+	pdm.train_all_items(retrain=True)
+	item_ids = [int(x.split('_')[1]) for x in pdm.price_columns]
+	for item_id in item_ids:
+		item_model = pdm.models[item_id]
+		model_dir = 'projects/{}/models'.format(project_id)
+		if not os.path.exists(model_dir):
+			os.makedirs(model_dir)
+		p.dump(item_model, open(model_dir + '/model_{}.p'.format(item_id),'wb'))
+	response_outp['result'] = 'Success'
+	
+	if cv_acc == True:
+		perf_df = pdm.get_all_performance()
+		response_outp['cv_acc'] = perf_df.to_json()
+
+	return jsonify(response_outp)
+
 
 @app.route('/predict/', methods=['POST'])
 def predict():
-    prices = request.json['prices']
-    project_id = request.get_json()['project_id']
+	prices = request.json['prices']
+	project_id = request.get_json()['project_id']
 
-    response_outp = {'prediction':0
-    }
+	response_outp = {'prediction':0
+	}
 
-    for k in list(prices.keys()):
-    	prices['Price_'+str(k)] = prices.pop(k)
+	for k in list(prices.keys()):
+		prices['Price_'+str(k)] = prices.pop(k)
 
-    prices = pd.DataFrame(prices, index=[0])
+	prices = pd.DataFrame(prices, index=[0])
 
-    models_list = os.listdir('projects/{}/models'.format(project_id))
-    items = [int(x.split('.')[0].split('_')[1]) for x in models_list]
+	models_list = os.listdir('projects/{}/models'.format(project_id))
+	items = [int(x.split('.')[0].split('_')[1]) for x in models_list]
 
-    pred_qty = {}
+	pred_qty = {}
 
-    for item in items:
-    	item_model = p.load(open('projects/{}/models/model_{}.p'.format(project_id,item),'rb'))
-    	prices = prices[item_model.get_booster().feature_names]
-    	pred_q = item_model.predict(prices)[0]
-    	pred_qty[item]=int(round(pred_q))
+	for item in items:
+		item_model = p.load(open('projects/{}/models/model_{}.p'.format(project_id,item),'rb'))
+		prices = prices[item_model.get_booster().feature_names]
+		pred_q = item_model.predict(prices)[0]
+		pred_qty[item]=int(round(pred_q))
 
-    response_outp['pred_q'] = pred_qty
-    print(pred_qty)
-    return jsonify(response_outp)
+	response_outp['pred_q'] = pred_qty
+	print(pred_qty)
+	return jsonify(response_outp)
 
 @app.route('/api/', methods=['POST'])
 def makecalc():
-    data = request.get_json()
-    prediction = np.array2string(model.predict(data))
+	data = request.get_json()
+	prediction = np.array2string(model.predict(data))
 
-    return jsonify(prediction)
+	return jsonify(prediction)
 
 if __name__ == '__main__':
-    modelfile = 'models/final_prediction.pickle'
-    model = p.load(open(modelfile, 'rb'))
-    app.run(debug=True, host='0.0.0.0')
+	modelfile = 'models/final_prediction.pickle'
+	model = p.load(open(modelfile, 'rb'))
+	app.run(debug=True, host='0.0.0.0')
--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import List
+import numpy as np
 
 
 def optimize_floats(df: pd.DataFrame) -> pd.DataFrame:
@@ -27,3 +28,17 @@ def optimize_objects(df: pd.DataFrame, datetime_features: List[str]) -> pd.DataF
 
 def optimize_memory(df: pd.DataFrame, datetime_features: List[str] = []):
     return optimize_floats(optimize_ints(optimize_objects(df, datetime_features)))
+
+def parse_training_request(request):
+	raw_data = request.files['file']
+	data_all = pd.read_csv(raw_data)
+	np.random.seed(42)
+	item_subset = list(np.random.choice(data_all['Item_ID'].unique(), size=10, replace=False))
+	data_subset = data_all.loc[data_all['Item_ID'].isin(item_subset)].copy()
+
+	data = data_subset
+
+	cv_acc = request.form['cv_acc']
+	project_id = request.form['project_id']
+
+	return (data, cv_acc, project_id)
